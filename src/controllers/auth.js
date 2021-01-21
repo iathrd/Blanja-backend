4d397f23200dd5e60f22ace26ef73a1a44790fe0
Iqbal Athorid <iqbalathoridd@gmail.com>
2021-01-21 12:05:03 +0800

    created customer user

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,17 +1,35 @@
+/* eslint-disable no-case-declarations */
 const { response } = require('../helpers/response')
 const { createSeller } = require('../helpers/validation')
+const { User } = require('../models')
+const argon = require('argon2')
 
 module.exports = {
   createUser: async (req, res) => {
     try {
       const { role } = req.params
       const data = await createSeller.validateAsync(req.body)
-      switch (role) {
-        case 'custommer':
-          break
+      const findEmail = await User.findOne({ where: { email: data.email } })
+      if (findEmail) {
+        response(res, `Email ${data.email} already registered!`)
+      } else {
+        const hashPassword = await argon.hash(data.password)
+        switch (role) {
+          case 'custommer':
+            const sendData = await User.create({ ...data, password: hashPassword })
+            if (sendData) {
+              response(res, 'Register succesfully', {
+                data: { ...sendData.dataValues, password: undefined }
+              })
+            } else {
+              response(res, 'Internal server error', {}, false, 500)
+            }
+            break
 
-        default:
-          break
+          default:
+            response(res, 'Not found', {}, false, 404)
+            break
+        }
       }
     } catch (error) {
       error.isJoi
--- src/helpers/validation.js
+++ src/helpers/validation.js
@@ -2,7 +2,7 @@ const Joi = require('joi')
 
 module.exports = {
   createSeller: Joi.object({
-    username: Joi.string().trim().min(5).required(),
+    name: Joi.string().trim().min(5).required(),
     email: Joi.string().trim().email().required(),
     password: Joi.string().min(8).required()
   })
