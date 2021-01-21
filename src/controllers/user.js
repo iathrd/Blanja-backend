27f4edf86c07c0e4da0d3a36fb8b51b316cfc9c3
Iqbal Athorid <iqbalathoridd@gmail.com>
2021-01-22 03:01:37 +0800

    fix bug

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,122 +1,141 @@
-const { response } = require('../helpers/response')
-const { updateUser, changePasswordSchema } = require('../helpers/validation')
-const { User, UserDetail } = require('../models')
-const argon = require('argon2')
+const { response } = require("../helpers/response");
+const { updateUser, changePasswordSchema } = require("../helpers/validation");
+const { User, UserDetail } = require("../models");
+const argon = require("argon2");
 
 module.exports = {
   updateUser: async (req, res) => {
     try {
-      const { role, id } = req.params
-      const { userId } = req.payload
+      const { role } = req.params;
+      const { userId } = req.payload;
       if (req.file !== undefined) {
-        let { path } = req.file
-        path = path.replace(/\\/g, '/')
+        let { path } = req.file;
+        path = path.replace(/\\/g, "/");
         req.body = {
           ...req.body,
-          avatar: path
-        }
+          avatar: path,
+        };
       }
-      const data = await updateUser.validateAsync(req.body)
-      const { name, email, password, ...userDetails } = data
-      const { avatar, gender, birthDay, storeName, ...userData } = data
+      const data = await updateUser.validateAsync(req.body);
+      const { name, email, password, ...userDetails } = data;
+      const { avatar, gender, birthDay, storeName, ...userData } = data;
       switch (role) {
-        case 'custommer':
+        case "custommer":
           if (Object.keys(userDetails).length && Object.keys(userData).length) {
-            const updateDetail = await UserDetail.update(userDetails, { where: { userId } })
+            const updateDetail = await UserDetail.update(userDetails, {
+              where: { userId },
+            });
             if (updateDetail) {
-              const updateUser = User.update(userData, { where: { id: userId } })
+              const updateUser = User.update(userData, {
+                where: { id: userId },
+              });
               if (updateUser) {
-                response(res, 'Update succesfully', { data: { ...updateDetail.dataValues, detailId: id } })
+                response(res, "Update succesfully", {
+                  data: { ...updateDetail.dataValuesd },
+                });
               } else {
-                response(res, 'Internal server error', {}, false, 500)
+                response(res, "Internal server error", {}, false, 500);
               }
             } else {
-              response(res, 'Internal server error', {}, false, 500)
+              response(res, "Internal server error", {}, false, 500);
             }
           } else if (Object.keys(userData).length) {
-            const updateUser = await User.update(userData, { where: { id: userId } })
+            const updateUser = await User.update(userData, {
+              where: { id: userId },
+            });
             if (updateUser) {
-              response(res, 'userUpdated')
+              response(res, "userUpdated");
             } else {
-              response(res, 'Internal server error', {}, false, 500)
+              response(res, "Internal server error", {}, false, 500);
             }
           } else {
-            const updateDetail = await UserDetail.update(userDetails, { where: { userId: id } })
+            const updateDetail = await UserDetail.update(userDetails, {
+              where: { userId },
+            });
             if (updateDetail) {
-              response(res, 'Update details sucesfully', { data: userDetails })
+              response(res, "Update details sucesfully", { data: userDetails });
             } else {
-              response(res, 'Internal server error', {}, false, 500)
+              response(res, "Internal server error", {}, false, 500);
             }
           }
-          break
+          break;
 
         default:
-          response(res, 'Not found', {}, 404)
-          break
+          response(res, "Not found", {}, 404);
+          break;
       }
     } catch (error) {
       error.isJoi
         ? response(res, error.message, {}, 400)
-        : response(res, 'Internal server error', {}, 500)
+        : response(res, "Internal server error", {}, 500);
     }
   },
   getUser: async (req, res) => {
     try {
-      const { userId } = req.payload
-      const user = await User.findOne({ include: [{ model: UserDetail, as: 'details' }], where: { id: userId } })
+      const { userId } = req.payload;
+      const user = await User.findOne({
+        include: [{ model: UserDetail, as: "details" }],
+        where: { id: userId },
+      });
       if (user) {
-        response(res, 'User detais', { data: user })
+        response(res, "User detais", { data: user });
       } else {
-        response(res, 'Internal server error', {}, false, 500)
+        response(res, "Internal server error", {}, false, 500);
       }
     } catch (error) {
-      console.log(error)
-      response(res, 'Internal server error', {}, false, 500)
+      console.log(error);
+      response(res, "Internal server error", {}, false, 500);
     }
   },
   deleteUser: async (req, res) => {
     try {
-      const { userId } = req.payload
-      const deleteDetails = await UserDetail.destroy({ where: { userId } })
-      console.log(deleteDetails)
+      const { userId } = req.payload;
+      const deleteDetails = await UserDetail.destroy({ where: { userId } });
+      console.log(deleteDetails);
       if (deleteDetails) {
-        const deleteUser = await User.destroy({ where: { id: userId } })
+        const deleteUser = await User.destroy({ where: { id: userId } });
         if (deleteUser) {
-          response(res, 'Delete succesfully')
+          response(res, "Delete succesfully");
         } else {
-          response(res, 'Internal server erro', {}, false, 500)
+          response(res, "Internal server erro", {}, false, 500);
         }
       } else {
-        response(res, 'Internal server erro', {}, false, 500)
+        response(res, "Internal server erro", {}, false, 500);
       }
     } catch (error) {
-      console.log(error)
-      response(res, 'Internal server error', {}, false, 500)
+      console.log(error);
+      response(res, "Internal server error", {}, false, 500);
     }
   },
   changePassword: async (req, res) => {
     try {
-      const { userId } = req.payload
-      const data = await changePasswordSchema.validateAsync(req.body)
-      const user = await User.findByPk(userId)
+      const { userId } = req.payload;
+      const data = await changePasswordSchema.validateAsync(req.body);
+      const user = await User.findByPk(userId);
       if (user) {
-        const validatePassword = await argon.verify(user.password, data.oldPassword)
+        const validatePassword = await argon.verify(
+          user.password,
+          data.oldPassword
+        );
         if (validatePassword) {
-          const hashedPassword = await argon.hash(data.newPassword)
-          const updatePassword = await User.update({ password: hashedPassword }, { where: { id: userId } })
+          const hashedPassword = await argon.hash(data.newPassword);
+          const updatePassword = await User.update(
+            { password: hashedPassword },
+            { where: { id: userId } }
+          );
           if (updatePassword) {
-            response(res, 'Change password succesfullt')
+            response(res, "Change password succesfullt");
           } else {
-            response(res, 'Failed to change password')
+            response(res, "Failed to change password");
           }
         }
       } else {
-        response(res, 'Internal server error', {}, false, 500)
+        response(res, "Internal server error", {}, false, 500);
       }
     } catch (error) {
       error.isJoi
         ? response(res, error.message, {}, false, 400)
-        : response(res, 'Internal server error', {}, false, 500)
+        : response(res, "Internal server error", {}, false, 500);
     }
-  }
-}
+  },
+};
--- src/routes/user.js
+++ src/routes/user.js
@@ -2,7 +2,7 @@ const route = require('express').Router()
 const user = require('../controllers/user')
 const { upload } = require('../helpers/uploadFile')
 
-route.patch('/updateUser/:role/:id', upload.single('avatar'), user.updateUser)
+route.patch('/updateUser/:role', upload.single('avatar'), user.updateUser)
 route.get('/getUser', user.getUser)
 route.delete('/deleteUser', user.deleteUser)
 route.patch('/changePassword', user.changePassword)
