009da5d7e3e43877ddd6404ac18b5ebf1c4f9f2c
Iqbal Athorid <iqbalathoridd@gmail.com>
2021-01-22 09:22:33 +0800

    creatd assosiation

--- src/models/product.js
+++ src/models/product.js
@@ -1,7 +1,5 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class Product extends Model {
     /**
@@ -11,18 +9,25 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      Product.hasMany(models.ProductImage, {
+        foreignKey: "productId",
+        as: "images",
+      });
     }
-  };
-  Product.init({
-    userId: DataTypes.INTEGER,
-    name: DataTypes.STRING,
-    description: DataTypes.TEXT,
-    brand: DataTypes.STRING,
-    size: DataTypes.STRING,
-    color: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'Product',
-  });
+  }
+  Product.init(
+    {
+      userId: DataTypes.INTEGER,
+      name: DataTypes.STRING,
+      description: DataTypes.TEXT,
+      brand: DataTypes.STRING,
+      size: DataTypes.STRING,
+      color: DataTypes.STRING,
+    },
+    {
+      sequelize,
+      modelName: "Product",
+    }
+  );
   return Product;
 };
